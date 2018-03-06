d74c130ca6b18469ff86f49c15e0850987dbabbf
zafatar <zaferkiziltoprak@gmail.com>
2018-03-06 23:07:10 +0100

    mapping package path updated

---
+++ src/main/java/com/zafatar/reservation/api/v1/model/mapping/ReservationJsonSerializer.java
@@ -0,0 +1,24 @@
+package com.zafatar.reservation.api.v1.model.mapping;
+
+import java.io.IOException;
+
+import org.springframework.boot.jackson.JsonComponent;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.zafatar.reservation.api.v1.model.Reservation;
+
+@JsonComponent
+public class ReservationJsonSerializer extends JsonSerializer<Reservation> {
+	@Override
+	public void serialize(Reservation reservation, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+			throws IOException, JsonProcessingException {
+		jsonGenerator.writeStartObject();
+		jsonGenerator.writeStringField("customer_name", reservation.getCustomerName());
+		jsonGenerator.writeStringField("from", reservation.getTimeSlot().getFrom());
+		jsonGenerator.writeStringField("to", reservation.getTimeSlot().getTo());
+		jsonGenerator.writeEndObject();
+	}
+}
